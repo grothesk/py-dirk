8eca40acf0949aa1ebde797fc8d635306f3229e0
Groth, Malte <malte.groth@deepshore.de>
2020-11-30 01:46:36 +0100

    Minor changes to README.md

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='py-dirk',
-    version='0.0.1',
+    version='0.0.2',
     description='dirk exports KUBECONFIG via direnv',
     long_description=long_description,
     long_description_content_type='text/markdown',
